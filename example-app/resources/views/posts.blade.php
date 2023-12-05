86873a88eb7f48dfbe9fb2049671c0aac03923b0
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2023-12-05 17:13:23 -0300

    laravel project initialized!

---
+++ example-app/resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="{{ asset('app.css') }}">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+</head>
+
+
+<body>
+    <article>
+        <h1>My first Laravel article</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, tenetur necessitatibus! Corrupti possimus, maiores sint voluptate consequuntur consectetur expedita, dolorum, quae ipsam recusandae culpa alias atque sequi aliquam esse porro?</p>
+    </article>
+</body>
+
+</html>
