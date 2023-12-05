98c6b1a67a6df9b9f88493e8cb5c12eb698cdd15
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2023-12-05 17:47:38 -0300

    routes added

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -20,3 +20,19 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return view('posts');
 });
+
+Route::get('/posts/{post}', function ($slug) {
+
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    ddd($path);
+    if (!file_exists($path)) {
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+});
